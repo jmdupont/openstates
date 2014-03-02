48be1abf804d8e42a7b13867c7cc932fe2a2080c
James Michael DuPont <jamesmikedupont@gmail.com>
2014-03-02 17:04:43 -0600

    flake8

--- openstates/ks/action_codes_scrape.py
+++ openstates/ks/action_codes_scrape.py
@@ -8,8 +8,12 @@ comment = re.compile(r'^#')
 variable = re.compile(r'\$([a-z_]+)\$')
 
 voted = re.compile(r'.*\$vote_tally\$.*')
-passed = re.compile(r'.*(?!not )(passed|adopted).*\$vote_tally\$.*', re.IGNORECASE)
-failed = re.compile(r'.*(failed|not adopted|not passed).*\$vote_tally\$.*', re.IGNORECASE)
+
+RE_PASSED = r'.*(?!not )(passed|adopted).*\$vote_tally\$.*'
+passed = re.compile(RE_PASSED, re.IGNORECASE)
+
+RE_FAILED = r'.*(failed|not adopted|not passed).*\$vote_tally\$.*'
+failed = re.compile(RE_FAILED, re.IGNORECASE)
 
 voted_codes = []
 passed_codes = []
@@ -17,38 +21,40 @@ failed_codes = []
 numbers = []
 new_numbers = {}
 
-def parse_action_codes(action_codes) :
-	with open(action_codes) as action_codes:
-		action_codes_str = action_codes.read()
-	for line in action_codes_str.split('\n'):
-		if comment.match(line):
-			continue
-		if data.match(line):
-			match = data.match(line)
-			number = match.group(1)
-			if number not in ksapi.action_codes:
-				print "New number: %s" % number
-				new_numbers[number] ="new"
 
-			numbers.append(number)
-			if voted.match(match.group(2)):
-				voted_codes.append(number)
-			elif passed.match(match.group(2)):
-				passed_codes.append(number)
-			elif failed.match(match.group(2)):
-				failed_codes.append(number)
-			else:
-				print "No rule %s" % line
+def parse_action_codes(action_codes):
+        with open(action_codes) as action_codes:
+                action_codes_str = action_codes.read()
+        for line in action_codes_str.split('\n'):
+                if comment.match(line):
+                        continue
+                if data.match(line):
+                        match = data.match(line)
+                        number = match.group(1)
+                        if number not in ksapi.action_codes:
+                                print "New number: %s" % number
+                                new_numbers[number] = "new"
 
-		else :
-			print "No match %s" % line
+                        numbers.append(number)
+                        if voted.match(match.group(2)):
+                                voted_codes.append(number)
+                        elif passed.match(match.group(2)):
+                                passed_codes.append(number)
+                        elif failed.match(match.group(2)):
+                                failed_codes.append(number)
+                        else:
+                                print "No rule %s" % line
 
-def report () :
-	print("voted = %s" % voted_codes)
-	print("passed = %s" % passed_codes)
-	print("failed = %s" % failed_codes)
+                else:
+                        print "No match %s" % line
+
+
+def report():
+        print("voted = %s" % voted_codes)
+        print("passed = %s" % passed_codes)
+        print("failed = %s" % failed_codes)
 
 
 if __name__ == '__main__':
-	parse_action_codes('action_codes')
-	report ()
+        parse_action_codes('action_codes')
+        report()
--- openstates/ks/bills.py
+++ openstates/ks/bills.py
@@ -9,10 +9,12 @@ import scrapelib
 
 from billy.scrape.bills import BillScraper, Bill
 from billy.scrape.votes import Vote
-from billy.scrape import NoDataForPeriod
 import logging
 _log = logging.getLogger('billy')
 import ksapi
+from billy.scrape import ScrapeError
+LI = 'http://www.kslegislature.org/li'
+
 
 class KSBillScraper(BillScraper):
     jurisdiction = 'ks'
@@ -30,19 +32,23 @@ class KSBillScraper(BillScraper):
         bill_request_json = json.loads(bill_request)
         bills = bill_request_json['content']
         for bill_data in bills:
-
             bill_id = bill_data['BILLNO']
-            
-
             if not(self.get_filter_bill_id() is False):
                 if not bill_id == self.get_filter_bill_id():
-                    _log.debug ("Skipping bill_id %s != %s" % (bill_id,self.get_filter_bill_id()))
+                    _log.debug("Skipping bill_id %s != %s" % (
+                        bill_id, self.get_filter_bill_id()))
                     continue
                 else:
-                    _log.debug ("Matched bill_id %s == %s" % (bill_id,self.get_filter_bill_id()))
+                    _log.debug(
+                        "Matched bill_id %s == %s" % (
+                            bill_id,
+                            self.get_filter_bill_id()))
             else:
-                _log.debug ("no check bill_id %s and %s" % (bill_id,self.get_filter_bill_id()))
-                
+                _log.debug(
+                    "no check bill_id %s and %s" % (
+                        bill_id,
+                        self.get_filter_bill_id())
+                )
 
             # filter other chambers
             if not bill_id.startswith(chamber_letter):
@@ -62,8 +68,9 @@ class KSBillScraper(BillScraper):
                         type=btype, status=bill_data['STATUS'])
             bill.add_source(ksapi.url + 'bill_status/' + bill_id.lower())
 
-            if (bill_data['LONGTITLE'] and
-                bill_data['LONGTITLE'] != bill['title']):
+            if (
+                    bill_data['LONGTITLE'] and
+                    bill_data['LONGTITLE'] != bill['title']):
                 bill.add_title(bill_data['LONGTITLE'])
 
             for sponsor in bill_data['SPONSOR_NAMES']:
@@ -77,7 +84,9 @@ class KSBillScraper(BillScraper):
                 actor = ('upper' if event['chamber'] == 'Senate'
                          else 'lower')
 
-                date = datetime.datetime.strptime(event['occurred_datetime'], "%Y-%m-%dT%H:%M:%S")
+                date = datetime.datetime.strptime(
+                    event['occurred_datetime'],
+                    "%Y-%m-%dT%H:%M:%S")
                 # append committee names if present
                 if 'committee_names' in event:
                     action = (event['status'] + ' ' +
@@ -99,21 +108,22 @@ class KSBillScraper(BillScraper):
             except scrapelib.HTTPError as e:
                 self.debug(e)
                 self.debug(bill)
-                self.warning('unable to fetch HTML for bill {0}'.format( bill['bill_id']))
-                
+                self.warning('unable to fetch HTML for bill {0}'.format(
+                    bill['bill_id']))
             self.save_bill(bill)
 
     def scrape_html(self, bill):
         slug = {'2013-2014': 'b2013_14'}[bill['session']]
         # we have to go to the HTML for the versions & votes
-        base_url = 'http://www.kslegislature.org/li/%s/measures/' % slug
+        base_url = '%s/%s/measures/' % (LI, slug)
         if 'resolution' in bill['type']:
-            base_url = 'http://www.kslegislature.org/li/%s/year1/measures/' % slug
+            base_url = '%s/%s/year1/measures/' % (LI, slug)
 
         url = base_url + bill['bill_id'].lower() + '/'
         data = self.urlopen(url)
         if len(data) < 1:
-            raise Exception("No data from bill id: %s on url %s" % (bill['bill_id'], url ))
+            raise Exception("No data from bill id: %s on url %s" % (
+                bill['bill_id'], url))
 
         doc = lxml.html.fromstring(data)
         doc.make_links_absolute(url)
@@ -134,10 +144,9 @@ class KSBillScraper(BillScraper):
                 if sn_url:
                     bill.add_document(title + ' - Supplementary Note', sn_url)
             if len(tds) > 3:
-                fn_url = get_doc_link(tds[3])
+                #UNUSED fn_url = get_doc_link(tds[3])
                 if sn_url:
                     bill.add_document(title + ' - Fiscal Note', sn_url)
-
 
         history_rows = doc.xpath('//tbody[starts-with(@id, "history-tab")]/tr')
         for row in history_rows:
@@ -163,7 +172,6 @@ class KSBillScraper(BillScraper):
                     amendment_name = row_text.strip()
                 bill.add_document(amendment_name, amendment)
 
-
     def parse_vote(self, bill, vote_date, vote_chamber, vote_status, vote_url):
         vote_chamber = 'upper' if vote_chamber == 'Senate' else 'lower'
         formats = ['%a %d %b %Y', '%b. %d, %Y, %H:%M %p',
@@ -178,12 +186,13 @@ class KSBillScraper(BillScraper):
         else:
             raise ValueError("couldn't parse date: " + vote_date)
 
-
         vote_doc, resp = self.urlretrieve(vote_url)
 
         try:
-            subprocess.check_call('timeout 10 abiword --to=ksvote.txt %s' % vote_doc,
-                                  shell=True, cwd='/tmp/')
+            subprocess.check_call(
+                'timeout 10 abiword --to=ksvote.txt %s' %
+                vote_doc,
+                shell=True, cwd='/tmp/')
         except subprocess.CalledProcessError:
             # timeout failed, some documents hang abiword
             self.error('abiword hung for longer than 10s on conversion')
@@ -198,8 +207,13 @@ class KSBillScraper(BillScraper):
         vote = None
         passed = True
         for line in vote_lines:
-            totals = re.findall('Yeas (\d+)[;,] Nays (\d+)[;,] (?:Present but not voting|Present and Passing):? (\d+)[;,] (?:Absent or not voting|Absent or Not Voting):? (\d+)',
-                                line)
+            totals = re.findall(
+                'Yeas (\d+)[;,] '
+                'Nays (\d+)[;,] '
+                '(?:Present but not voting|Present and Passing):? '
+                '(\d+)[;,] '
+                '(?:Absent or not voting|Absent or Not Voting):? (\d+)',
+                line)
             line = line.strip()
             if totals:
                 totals = totals[0]
